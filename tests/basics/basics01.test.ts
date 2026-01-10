de075b8326f2f5d7d2bd5914572038905e37c200
seveibar <seveibar@gmail.com>
2026-01-10 12:44:10 -0800

    add basics01

---
+++ tests/basics/basics01.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "bun:test"
+import { getSvgFromGraphicsObject } from "graphics-debug"
+import { CurvyTraceSolver } from "lib/CurvyTraceSolver"
+import problem from "fixtures/basics/basics01-input.json"
+import type { CurvyTraceProblem } from "lib/types"
+
+test("basics01", () => {
+  const solver = new CurvyTraceSolver(problem as CurvyTraceProblem)
+  solver.solve()
+  const svg = getSvgFromGraphicsObject(solver.visualize())
+  expect(svg).toMatchSvgSnapshot(import.meta.path)
+})
