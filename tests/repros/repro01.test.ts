5ab8f17bbd1b2d0943f382c75d80495bb11086d7
seveibar <seveibar@gmail.com>
2026-01-10 19:49:29 -0800

    add score logging

--- tests/repros/repro01.test.ts
+++ tests/repros/repro01.test.ts
@@ -3,10 +3,19 @@ import { getSvgFromGraphicsObject } from "graphics-debug"
 import { CurvyTraceSolver } from "lib/CurvyTraceSolver"
 import problem from "./repro01-input.json"
 import type { CurvyTraceProblem } from "lib/types"
+import { scoreOutputCost } from "lib/scoreOutputCost"
 
 test("repro01", () => {
   const solver = new CurvyTraceSolver(problem as CurvyTraceProblem)
   solver.solve()
+
+  console.log(
+    "cost:",
+    scoreOutputCost({
+      problem: problem as CurvyTraceProblem,
+      outputTraces: solver.outputTraces,
+    }),
+  )
   const svg = getSvgFromGraphicsObject(solver.visualize())
   expect(svg).toMatchSvgSnapshot(import.meta.path)
 })
