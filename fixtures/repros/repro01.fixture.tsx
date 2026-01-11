aeb638d3cbb128fefc026d66372d7861afc1d861
seveibar <seveibar@gmail.com>
2026-01-10 19:48:41 -0800

    add repro

---
+++ fixtures/repros/repro01.fixture.tsx
@@ -0,0 +1,11 @@
+import { GenericSolverDebugger } from "@tscircuit/solver-utils/react"
+import type { CurvyTraceProblem } from "../../lib/types"
+import { CurvyTraceSolver } from "../../lib/CurvyTraceSolver"
+import problem from "../../tests/repros/repro01-input.json"
+export default () => {
+  return (
+    <GenericSolverDebugger
+      createSolver={() => new CurvyTraceSolver(problem as CurvyTraceProblem)}
+    />
+  )
+}
